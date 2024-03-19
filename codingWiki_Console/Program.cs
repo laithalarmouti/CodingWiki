abf223204f5c86a7042e2805e89c950fc44d44e4
laith alarmouti <laithalarmouti@gmail.com>
2024-03-19 13:06:03 +0300

    Push1

--- codingWiki_Console/Program.cs
+++ codingWiki_Console/Program.cs
@@ -16,14 +16,17 @@ Console.WriteLine("Hello, World!");
 //    }
 //}
 
-getAllBooks();
+//getAllBooks();
 //addBook();
-getBook();
+//getBook();
+//updateBook();
+deleteBook();
 
-void getAllBooks()
+
+async void getAllBooks()
 {
     using var context = new ApplicationDbContext();
-    var books = context.Books.ToList();
+    var books = await context.Books.ToListAsync();
     foreach(var book in books)
     {
         Console.WriteLine(book.Title + "-" + book.ISBN);
@@ -32,29 +35,61 @@ void getAllBooks()
 
 
 
-void addBook()
+async void addBook()
 {
-    Book book = new() {Title= "Ant Without Duty", ISBN= "48XQ2", Price= 44.22m, Publisher_Id= 4345 };
+    Book book = new() {Title= "New Ef Core Book", ISBN= "99999", Price= 14.22m, Publisher_Id= 2455 };
     using var context = new ApplicationDbContext();
-    var books = context.Books.Add(book);
-    context.SaveChanges();
+    var books = await context.Books.AddAsync(book);
+    await context.SaveChangesAsync();
    
 }
 
-void getBook()
+async void getBook()
 {
     try
     {
         using var context = new ApplicationDbContext();
-        var books = context.Books;
+        var books = await context.Books.Skip(0).Take(2).ToListAsync();
         //Console.WriteLine(book.Title + "-" + book.ISBN);
         foreach (var book in books)
         {
             Console.WriteLine(book.Title + "-" + book.ISBN);
+        }
+
+        books = await context.Books.Skip(4).Take(1).ToListAsync();
+        foreach (var book in books)
+        {
+            Console.WriteLine(book.Title + " - " + book.ISBN);
         }
     }
     catch (Exception e)
     {
         Console.WriteLine(e.Message);
     } 
+}
+
+async void updateBook()
+{
+    try
+    {
+        using var context = new ApplicationDbContext();
+        var books = await context.Books.Where(u=> u.Publisher_Id==1).ToListAsync();
+        foreach (var book in books)
+        {
+            book.Price = 55.55m;
+        }
+        await context.SaveChangesAsync();  
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+async void deleteBook()
+{
+    using var context = new ApplicationDbContext();
+    var books = await context.Books.FindAsync(10);
+    context.Books.Remove(books);
+    await context.SaveChangesAsync() ; 
 }
